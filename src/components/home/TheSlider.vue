3328b8fcc3aeda28c78f283088dcf0d75515eb12
Natnael Abay <se.natnael.abay@gmail.com>
2021-05-21 17:10:04 +0300

    slider component added

---
+++ src/components/home/TheSlider.vue
@@ -0,0 +1,134 @@
+<template>
+  <main>
+    <section class="slider-area pos-relative">
+      <div class="slider-active">
+        <div
+          class="single-slider slide-1-style slide-height d-flex align-items-center"
+          data-background="../../assets/img/slider/slide1.jpg"
+        > 
+          <div class="shape-title bounce-animate">
+            <h2>UX</h2>
+          </div>
+          <div class="shape-icon bounce-animate">
+            <img src="../../assets/img/slider/shape-icon.png" alt="" />
+          </div>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-xl-7">
+                <div class="slide-content">
+                  <span data-animation="fadeInRight" data-delay=".2s"
+                    >Furniture Collection</span
+                  >
+                  <h1 data-animation="fadeInUp" data-delay=".5s">
+                    Synnes Dining Chair Upholstered
+                  </h1>
+                  <div class="slide-btn">
+                    <a
+                      class="btn theme-btn"
+                      href="shop.html"
+                      data-animation="fadeInLeft"
+                      data-delay=".7s"
+                      >shop now</a
+                    >
+                    <a
+                      class="btn white-btn"
+                      href="shop.html"
+                      data-animation="fadeInRight"
+                      data-delay=".9s"
+                      >category</a
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-5">
+                <div
+                  class="slide-shape1"
+                  data-animation="bounceInRight"
+                  data-delay=".9s"
+                >
+                  <img src="../../assets/img/slider/shape1.png" alt="" />
+                </div>
+                <div
+                  class="slide-shape2"
+                  data-animation="bounceInRight"
+                  data-delay="1.2s"
+                >
+                  <img src="../../assets/img/slider/shape2.png" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="single-slider slide-1-style slide-height d-flex align-items-center"
+          data-background="../../assets/img/slider/slide1.jpg"
+        > 
+          <div class="shape-title bounce-animate">
+            <h2>UX</h2>
+          </div>
+          <div class="shape-icon bounce-animate">
+            <img src="../../assets/img/slider/shape-icon.png" alt="" />
+          </div>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-xl-7">
+                <div class="slide-content">
+                  <span data-animation="fadeInRight" data-delay=".2s"
+                    >Furniture Collection</span
+                  >
+                  <h1 data-animation="fadeInUp" data-delay=".5s">
+                    Synnes Dining Chair Upholstered
+                  </h1>
+                  <div class="slide-btn">
+                    <a
+                      class="btn theme-btn"
+                      href="shop.html"
+                      data-animation="fadeInLeft"
+                      data-delay=".7s"
+                      >shop now</a
+                    >
+                    <a
+                      class="btn white-btn"
+                      href="shop.html"
+                      data-animation="fadeInRight"
+                      data-delay=".9s"
+                      >category</a
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-5">
+                <div
+                  class="slide-shape1"
+                  data-animation="bounceInRight"
+                  data-delay=".9s"
+                >
+                  <img src="../../assets/img/slider/shape1.png" alt="" />
+                </div>
+                <div
+                  class="slide-shape2"
+                  data-animation="bounceInRight"
+                  data-delay="1.2s"
+                >
+                  <img src="../../assets/img/slider/shape2.png" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+  props: {},
+};
+</script>
