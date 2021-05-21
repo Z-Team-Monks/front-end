ad9516f10b532af094fcec9d1b02696b65dcde51
Natnael Abay <se.natnael.abay@gmail.com>
2021-05-21 17:16:59 +0300

    some fixes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
-  <v-app>
-    <v-app-bar app color="light" dark>
-     <h1>Home Page</h1>
-    </v-app-bar>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <div>
+    <TheHeader />
+    <router-view />
+    <TheFooter />
+  </div>
 </template>
 
 <script>
+import TheHeader from "@/components/common/TheHeader";
+import TheFooter from "@/components/common/TheFooter";
 export default {
   name: "App",
-
+  components: {
+    TheHeader,
+    TheFooter,
+  },
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+
+@import url("assets/css/bootstrap.min.css");
+/* @import url("assets/css/owl.carousel.min.css"); */
+@import url("assets/css/animate.min.css");
+@import url("assets/css/magnific-popup.css");
+@import url("assets/css/fontawesome-all.min.css");
+@import url("assets/css/flaticon.css");
+@import url("assets/css/meanmenu.css");
+@import url("assets/css/slick.css");
+@import url("assets/css/default.css");
+@import url("assets/css/style.css");
+@import url("assets/css/responsive.css"); 
+</style>
