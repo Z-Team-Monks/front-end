fc13841f931614c2ee02454012579ac7cd389c90
Natnael Abay <se.natnael.abay@gmail.com>
2021-05-21 17:10:45 +0300

    cart view /page setup/

---
+++ src/views/cart/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Cart />
+  </div>
+</template>
+<script>
+import Cart from "@/components/cart/index";
+export default {
+  components: {
+    Cart,
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+  props: {},
+};
+</script>
