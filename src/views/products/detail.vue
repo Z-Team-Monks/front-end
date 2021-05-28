97f64f3bc0f6b653d0c316204c18ee49f095de78
Natnael Abay <se.natnael.abay@gmail.com>
2021-05-28 22:47:33 +0300

    product detail file created

---
+++ src/views/products/detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>product Detail</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+  props: {},
+};
+</script>
