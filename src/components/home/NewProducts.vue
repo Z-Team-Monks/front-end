bfdb5881c5888dd15866691742bc57a5226ab416
Natnael Abay <se.natnael.abay@gmail.com>
2021-05-29 18:45:20 +0300

    show product detail modal

--- src/components/home/NewProducts.vue
+++ src/components/home/NewProducts.vue
@@ -67,7 +67,7 @@
           </div>
         </div>
       </div>
-    </div> 
+    </div>
     <div class="row">
       <div class="col-xl-12">
         <div class="product-tab-content">
@@ -76,20 +76,20 @@
               <slide>
                 <div class="ml-2 row">
                   <div class="col">
-                    <ProductCard />
+                    <ProductCard @emitProductDetail="showDetailModal" />
                   </div>
                   <div class="col">
-                    <ProductCard />
+                    <ProductCard @emitProductDetail="showDetailModal" />
                   </div>
                 </div>
               </slide>
               <slide>
                 <div class="ml-2 row">
                   <div class="col">
-                    <ProductCard />
+                    <ProductCard @emitProductDetail="showDetailModal" />
                   </div>
                   <div class="col">
-                    <ProductCard />
+                    <ProductCard @emitProductDetail="showDetailModal" />
                   </div>
                 </div>
               </slide>
@@ -98,6 +98,83 @@
         </div>
       </div>
     </div>
+
+    <!--  do not be lazy nati move it to its own component -->
+    <v-row justify="center">
+      <v-dialog v-model="dialog" max-width="850">
+        <v-card class="p-3">
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              icon
+              x-small
+              color="green darken-1"
+              text
+              @click="dialog = !dialog"
+            >
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-row>
+            <v-col cols="5">
+              <v-img
+                contain
+                lazy-src="https://picsum.photos/id/11/10/6"
+                max-height="300"
+                max-width="300"
+                src="https://picsum.photos/id/11/500/300"
+              ></v-img>
+            </v-col>
+            <!-- <v-col cols="1" /> -->
+            <v-col cols="6">
+              <div class="details-cat mb-20">
+                <a href="#">decor,</a>
+                <a href="#">furniture</a>
+              </div>
+              <h2 class="pro-details-title">Limonda Women</h2>
+              <div class="details-price mb-20">
+                <span>$119.00</span>
+                <span class="old-price">$246.00</span>
+              </div>
+
+              <div class="product-desc variant-item">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip.
+                </p>
+              </div>
+              <v-row>
+                <v-col cols="4">
+                  <v-btn fab elevation="0" color="red lighten-4" x-small>
+                    <v-icon small> mdi-arrow-up </v-icon>
+                  </v-btn>
+                  <p class="mx-2 d-inline-block">1</p>
+                  <v-btn fab elevation="0" color="red lighten-4" x-small>
+                    <v-icon small> mdi-arrow-down </v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col>
+                  <v-btn
+                    class="ml-2"
+                    outlined
+                    color="red lighten--4"
+                    dark
+                    tile
+                    block
+                  >
+                    <v-icon left>mdi-cart</v-icon>
+                    Add To Cart
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-dialog>
+    </v-row>
+    <!-- end -->
   </section>
 </template>
 <script>
@@ -109,13 +186,27 @@ export default {
     ProductCard,
     Carousel,
     Slide,
+    // ProductDetailModal,
   },
   data() {
-    return {};
+    return {
+      dialog: false,
+    };
   },
   created() {},
-  computed: {},
-  methods: {},
+  computed: {
+    modalShow() {
+      return this.dialog;
+    },
+  },
+  methods: {
+    showDetailModal(a) {
+      this.dialog = true;
+    },
+    closeModal(e) {
+      this.dialog = false;
+    },
+  },
   props: {},
 };
 </script>
